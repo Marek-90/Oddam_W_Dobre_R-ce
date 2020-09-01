cfcf56a73f9089a6f66a851e336350f6c212309e
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-01 06:37:04 +0200

    Router

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import Home from "./components/Home";
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route component={Home} exact path="/" />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 export default App;
