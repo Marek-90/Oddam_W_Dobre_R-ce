3e31b307d4c498c7924811c418bddcbe36104d7a
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-02 11:39:23 +0200

    style

--- my-app/src/components/Contact/Person.jsx
+++ my-app/src/components/Contact/Person.jsx
@@ -5,11 +5,11 @@ const Person = () => {
     <div className="person">
       <div>
         <p>Wpisz swoje imię</p>
-        <input type="text" />
+        <input type="text" placeholder="Krzysztof" />
       </div>
       <div>
         <p>Wpisz swój email</p>
-        <input type="text" />
+        <input type="text" placeholder="abc@xyz.pl" />
       </div>
     </div>
   );
