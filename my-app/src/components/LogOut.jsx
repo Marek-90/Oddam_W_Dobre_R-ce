f095fa640deef20812d2e8ebed0066953c3cd408
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-10 15:20:54 +0200

    logOut panel

--- my-app/src/components/LogOut.jsx
+++ my-app/src/components/LogOut.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Login from "./Header/Login";
 import Navigation from "./Header/Navigation";
 import Decoration from "../assets/Decoration.svg";
 
 const LogOut = () => {
   return (
-    <div className="navLog">
-      <Login />
-      <Navigation />
+    <>
+      <div className="navLog">
+        <Login />
+        <Navigation />
+      </div>
       <div className="logOut">
         <h1 className="logOut__title">Wylogowanie nastąpiło pomyślnie!</h1>
         <img className="goHelp__img" src={Decoration} alt="Decoration" />
+        <Link to="/" className="logOut__btn">
+          Strona główna
+        </Link>
       </div>
-    </div>
+    </>
   );
 };
 
