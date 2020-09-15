f6e4f0ef4365515d02acc77d98ea87d655f02928
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-15 07:14:00 +0200

    login support

--- my-app/src/components/LogRegister/LogFormInput.jsx
+++ my-app/src/components/LogRegister/LogFormInput.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LogFormInput = () => {
+  const [logEmailError, setlogEmailError] = useState(null);
+  const [logPasswordError, setLogPasswordError] = useState(null);
+
   return (
     <div className="logFormInput">
       <p className="logFormInput__input">Email</p>
       <input type="text" />
+      <p className="errorStyle">{logEmailError}</p>
       <p className="logFormInput__input">Hasło</p>
       <input type="password" />
+      <p className="errorStyle">{logPasswordError}</p>
     </div>
   );
 };
