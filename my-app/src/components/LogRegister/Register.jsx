5882cd08ae2c780912533cead8d5a9af16193e2d
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-16 06:33:51 +0200

    support register

--- my-app/src/components/LogRegister/Register.jsx
+++ my-app/src/components/LogRegister/Register.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import RegisterInput from "./RegisterInput";
 import Decoration from "../../assets/Decoration.svg";
 
 const Register = () => {
+  const [registerErrorEmail, setRegisterErrorEmail] = useState(null);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    repPassword: "",
+  });
+
+  const reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  const supportRegisterError = (data) => {
+    setRegisterErrorEmail(null);
+    if (!data.email) {
+      return "Podaj email";
+    } else if (!reg.test(data.email)) {
+      return "Niepoprawny e-mail";
+    }
+    return null;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("dupa");
+    console.log(data);
+
+    const supportRegisterErrorS = supportRegisterError(data);
+
+    if (supportRegisterErrorS) {
+      supportRegisterError(supportRegisterErrorS);
+    }
+  };
+
   return (
-    <div className="logForm">
+    <form onSubmit={handleSubmit} className="logForm">
       <h1 className="logForm__title">Załóż konto</h1>
       <img className="goHelp__img" src={Decoration} alt="Decoration" />
-      <RegisterInput />
+      <RegisterInput registerErrorEmail={registerErrorEmail} />
       <div>
         <div className="logForm__btn">
           <Link to="/logowanie">Zaloguj się</Link>
           <button>Załóż konto</button>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
--- my-app/src/components/LogRegister/RegisterInput.jsx
+++ my-app/src/components/LogRegister/RegisterInput.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const RegisterInput = () => {
+const RegisterInput = ({ registerErrorEmail }) => {
   return (
     <div className="logRegInput">
       <p className="logFormInput__input">Email</p>
       <input type="text" />
+      <p className="errorStyle">{registerErrorEmail}</p>
       <p className="logFormInput__input">Hasło</p>
       <input type="password" />
       <p className="logFormInput__input">Powtórz hasło</p>
